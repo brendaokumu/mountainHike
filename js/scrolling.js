e8a970957baf4470905012c57891903aa5df0528
brendaokumu <brenda.okumu2@gmail.com>
2019-07-25 15:14:58 +0300

    add scrolling features

---
+++ js/scrolling.js
@@ -0,0 +1,22 @@
+$(function () {     //jquery DOM function
+    $(window).on("load resize", function () {
+        $(".fill-screen").css("height", window.innerHeight);
+    });
+
+    // add Bootstrap's scrollspy
+   $('body').scrollspy({
+       target: '.navbar',
+       offset: 160
+   });
+
+   // smooth scrolling
+   $('nav a, .down-button a').bind('click', function () {
+       $('html, body').stop().animate({
+           scrollTop: $($(this).attr('href')).offset().top - 100
+       }, 1500, 'easeInOutExpo');
+       event.preventDefault();
+   });
+
+   // parallax scrolling with stellar.js
+   $(window).stellar(); //select our window then select our stellar function
+});
